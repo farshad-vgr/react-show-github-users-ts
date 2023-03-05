44599ca8cfa29175edffed23178ebe72b568811d
farshad-vgr <farshadvaghari@gmail.com>
2023-03-05 20:20:12 +0330

    Icons added to no-item-found

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import "./App.css";
 const App = () => {
 	const ul = useRef();
 	const numberOfUsers = useRef(10); // If not set default value is 30
-	let [hiddenItems, setHiddenItems] = useState(0);
-	const [requestNumber, setRequestNumber] = useState(60);
+	const [hiddenItems, setHiddenItems] = useState(0);
+	const [requestNumber, setRequestNumber] = useState(60); // Max number of requests is 60
 	const { userName } = useContext(myContext);
 
 	// This function handles loading a list of users with a smooth-fading animation(adds a specific class to all items)
@@ -63,7 +63,7 @@ const App = () => {
         `;
 				deleterSpan.addEventListener("click", () => {
 					if (window.confirm(`Are you sure you want to delete "${user.login}" from list??`)) {
-            const arrayOfLi = [ ...ul.current.querySelectorAll("li") ];
+						const arrayOfLi = [...ul.current.querySelectorAll("li")];
 
 						for (const eachLi of arrayOfLi) {
 							if (eachLi.querySelector("h3").innerText === `${user.login}`) {
@@ -167,7 +167,21 @@ const App = () => {
 
 			{hiddenItems === numberOfUsers.current && (
 				<div className="no-item">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" width="4rem" height="4rem" stroke="currentColor">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+						/>
+					</svg>
 					<h1>No Item Found !</h1>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" width="4rem" height="4rem" stroke="currentColor">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+						/>
+					</svg>
 				</div>
 			)}
 		</section>
